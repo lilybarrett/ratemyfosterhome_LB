6b81e53ad6c35a3796493555d81887a4f690f5a5
lilybarrett <lily@barrett.us>
2016-03-18 16:46:23 -0400

    create users and manually set up initial admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :users, only: [:index, :show]
+
+  get "home", to: "home#show"
+
+  root 'home#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
