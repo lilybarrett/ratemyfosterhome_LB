a9989ddc571421262f8b6f0808d12d0123adddda
lilybarrett <lily@barrett.us>
2016-03-19 17:18:56 -0400

    added and passed tests for user deletes account

---
+++ spec/features/users/user_deletes_account_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature "user deletes account" do
+
+  scenario "user successfully deletes account" do
+    user = FactoryGirl.create(:user)
+    visit new_user_session_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Sign In"
+
+    click_link "Edit Account"
+    click_button "Delete My Account"
+
+    expect(page).to have_content("Bye! Your account has been successfully cancelled")
+
+    visit new_user_session_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign In"
+
+    expect(page).to have_content("Invalid email or password")
+    expect(page).to have_content("Sign In")
+  end
+end
