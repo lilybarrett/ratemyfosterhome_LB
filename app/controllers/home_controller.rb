ad4fea65a161e1b637ff2bf7da74edf40c59d566
lilybarrett <lily@barrett.us>
2016-05-07 17:52:07 -0400

    add admin dashboard functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def show
     @user_results = User.categories_for_user_dropdown
+    @foster_home = FosterHome.new
+    @child_results = FosterKid.categories_for_child_dropdown
+    @parent_results = FosterParent.categories_for_parent_dropdown
+
+    @foster_kid = FosterKid.new
+    @foster_parent = FosterParent.new
   end
 end
