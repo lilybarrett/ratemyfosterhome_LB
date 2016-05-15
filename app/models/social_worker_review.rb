430498e314f6553fad09f99cf3c6180e6c053c46
lilybarrett <lily@barrett.us>
2016-05-15 12:23:08 -0400

    add validations for foster home ratings

--- app/models/social_worker_review.rb
+++ app/models/social_worker_review.rb
@@ -3,7 +3,9 @@ class SocialWorkerReview < ActiveRecord::Base
   belongs_to :foster_home
 
   validates :rating, presence: true, numericality: {
-    only_integer: true
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 10
   }
   validates :comment, presence: true
 
